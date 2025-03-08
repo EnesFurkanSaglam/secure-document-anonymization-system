5c3db6225cc86d2a6fedc67447d79a9603b3275b
Enes Furkan Saglam <enesfurkansaglam@gmail.com>
2025-03-08 16:19:59 +0300

    Add editor_service

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -2,11 +2,13 @@ from .anon_service import AnonService
 from .author_service import AuthorService
 from .file_service import FileService
 from .reviewer_service import ReviweverService
+from .editor_service import EditorService
 
 anon_service = AnonService()
 author_service = AuthorService()
 file_service = FileService()
 reviewer_service = ReviweverService()
+editor_service = EditorService()
 
 
-__all__ = ["anon_service", "author_service", "file_service","reviewer_service"]
+__all__ = ["anon_service", "author_service", "file_service","reviewer_service","editor_service"]
