0f701cbdd706d6934d72014f4b7997fefb66786c
Enes Furkan Saglam <enesfurkansaglam@gmail.com>
2025-03-08 15:44:43 +0300

    Add new coumn in review

--- backend/models/review.py
+++ backend/models/review.py
@@ -6,11 +6,12 @@ class Review(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     assignment_id = db.Column(db.Integer, db.ForeignKey('article_assignments.id'), nullable=False)
+    
     review_text = db.Column(db.Text, nullable=True)
+    
+    review_pdf_path = db.Column(db.String(255), nullable=True)
+
     submitted_at = db.Column(db.DateTime, default=datetime.utcnow)
-    is_final = db.Column(db.Boolean, default=False)  # final mi?
+    is_final = db.Column(db.Boolean, default=False)
 
     assignment = db.relationship("ArticleAssignment", back_populates="reviews")
-
-    def __repr__(self):
-        return f"<Review {self.id}>"
