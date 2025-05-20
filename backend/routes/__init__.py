874a9210282e055bea26110c26e26cd6c1910ab1
Enes Furkan Saglam <enesfurkansaglam@gmail.com>
2025-05-20 22:39:58 +0300

    Removed __pycache__ from tracking

--- backend/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from .author_routes import author_bp
-from .editor_routes import editor_bp
-from .reviewer_routes import reviewer_bp
-
-__all__ = ["author_bp", "editor_bp", "reviewer_bp", "user_bp"]
